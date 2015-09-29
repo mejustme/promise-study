c6de412c642ec69fc2d12a9ecd926f2212d65a61
lianxu <qinhui.cqh@alibaba-inc.com>
2015-09-29 20:08:36 +0800

    update ok

--- js/index.js
+++ js/index.js
@@ -1,56 +1,32 @@
 var when = require('../bower_components/when/when.js');
 var arr = [1,2,3,4,5,6];
-var arrPromise = [];
-function run(i){
-    var deferred = when.defer();
+var deferred = when.defer();
+function run(deferred){
     var start = 1, end = 100;
-    var beginTime;
-    (function(interval) {
-        beginTime = new Date();
-        setInterval(function(){
-            if(start<end){
-                deferred.notify(start++);
-
-            }else{
-                deferred.notify(start++);
-                var obj = {
-                    beginTime: beginTime,
-                    endTime: new Date()
-                };
-                deferred.resolve(obj);//只能传递一个对象
-            }
-        },interval);
-    })(i*30);
+    setInterval(function(){
+        start <= end ? deferred.notify(start++) : '';
+        start == end ? deferred.resolve() : ""
+    },Math.random()*1000);
     return deferred.promise;
 }
-arr.forEach(function(value,i){
-    var promise = run(value);
-    promise.then(endTask(value),null,changeText(value));
-    arrPromise.push(promise);
+var promise = run(deferred);
+arr.forEach(function(value) {
+    promise = promise.then(done(value),null,underway(value));
+    //then()函数返回一个新的'promise',具体状态与值由里面的回调函数返回值决定。
 })
 
-function changeText(value){
-    return function(content){
-        $('#content'+ value).text('进度:' + content + '%');
-    }
-
-}
-
-function endTask(value){
-    return function(obj){
-        console.log("第"+ value+ "个任务完成了! 用时：" + (obj.endTime-obj.beginTime) + " ms");
+function done(i) {
+    return function(value) {
+        console.log("第" + i + "个任务完成!");
+        var deferred = when.defer();
+        return run(deferred);
+        //当不返回，或者返回非promise对象，执行正确回调
+        //当返回pormise对象，根据这个promise状态执行响应函数，当状态未定，则等待，直到确定才执行回调函数
     }
 }
 
-function endAllTask(arr){
-    console.log(arr);
-    alert("任务完成");
+function underway(i) {
+    return function(progress) {
+        $("#content" + i).text("进度: " + progress + "%");
+    }
 }
-
-function notendAllTask(arr){
-    console.log(arr);
-    alert("有任务出错" + arr.toString());
-}
-
-when.all(arrPromise).then(endAllTask,notendAllTask);
-//when.some(arrPromise,3).then(endAllTask,notendAllTask);
