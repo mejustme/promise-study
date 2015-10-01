75e1c1544b5f6764705ac969c12170166daeecb4
lianxu <qinhui.cqh@alibaba-inc.com>
2015-10-01 12:43:18 +0800

    jquery derferred

--- js/index.js
+++ js/index.js
@@ -1,13 +1,12 @@
-var when = require('../bower_components/when/when.js');
 var arr = [1,2,3,4,5,6];
-var deferred = when.defer();
+var deferred = $.Deferred();
 function run(deferred){
     var start = 1, end = 100;
     setInterval(function(){
         start <= end ? deferred.notify(start++) : '';
         start > end ? deferred.resolve() : ""
     },Math.random()*1000);
-    return deferred.promise;
+    return deferred.promise();
 }
 var promise = run(deferred);
 arr.forEach(function(value) {
@@ -21,7 +20,7 @@ arr.forEach(function(value) {
 function done(i) {
     return function(value) {
         console.log("第" + i + "个任务完成!");
-        var deferred = when.defer();
+        var deferred = $.Deferred();
         return run(deferred);
         //当不返回，或者返回非promise对象，执行正确回调
         //当返回pormise对象，根据这个promise状态执行响应函数（成功/失败），当状态未定，则等待，直到确定才执行回调函数
